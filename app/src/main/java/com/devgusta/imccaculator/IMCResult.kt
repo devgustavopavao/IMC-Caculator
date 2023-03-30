1193c8788e09101cd3ddc80e1e85e5f05b5b9977
devgustavopavao <contact.gustavopavao@gmail.com>
2023-03-30 16:35:23 -0300

    Finish The APP #by devgustavopavao

--- app/src/main/java/com/devgusta/imccaculator/IMCResult.kt
+++ app/src/main/java/com/devgusta/imccaculator/IMCResult.kt
@@ -1,5 +1,6 @@
 package com.devgusta.imccaculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.devgusta.imccaculator.databinding.ActivityImcresultBinding
@@ -7,9 +8,19 @@ import com.devgusta.imccaculator.databinding.ActivityMainBinding
 
 class IMCResult : AppCompatActivity() {
     private lateinit var binding: ActivityImcresultBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityImcresultBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //recuperando valores
+        val bundle = intent.extras
+        val peso = bundle?.getDouble("peso")
+        val altura = bundle?.getDouble("altura")
+        val result = bundle?.getString("result")
+        binding.textPeso.text = "PESO: ${peso.toString()} KG"
+        binding.textAltura.text = "ALTURA: ${altura.toString()}"
+        binding.textDiagnostico.text = "$result"
     }
 }
--- app/src/main/java/com/devgusta/imccaculator/MainActivity.kt
+++ app/src/main/java/com/devgusta/imccaculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devgusta.imccaculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.devgusta.imccaculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +16,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startAction(){
+        val intent = Intent(this,IMCResult::class.java)
         binding.buttonCalculator.setOnClickListener {
-            val intent = Intent(this,IMCResult::class.java)
-            startActivity(intent)
+            val sPeso: String = binding.editText1.text.toString()
+            val sAltura: String = binding.editText2.text.toString()
+            if(sPeso.isNotEmpty()){
+              val peso =  sPeso.toDouble()
+                if(sAltura.isNotEmpty()){
+                   val altura = sAltura.toDouble()
+                    val result = peso / (altura * altura)
+                   if(result <18){
+                       intent.putExtra("result","Abaixo do peso")
+                   }else if(result in 18.5..24.9){
+                       intent.putExtra("result","Peso ideal ;)")
+                   }else if(result in 25.0..29.9){
+                       intent.putExtra("result", "Acima do peso")
+                   }else{
+                       intent.putExtra("result","Obesidade")
+                   }
+                    intent.putExtra("peso",peso)
+                    intent.putExtra("altura",altura)
+                    startActivity(intent)
+                }else{
+                    with(binding.editText2){
+                        setError("Digite algo antes")
+                        requestFocus()
+                    }
+                }
+            }else{
+                with(binding.editText1){
+                    setError("Digite algo antes")
+                    requestFocus()
+                }
+            }
+
 
         }
     }
